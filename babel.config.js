f8d14122686ddad727c969bb23c0d10ca3c8cf14
Italo Menezes <italomlporoca@hotmail.com>
2019-12-14 06:29:22 -0300

    fix tron statements in production

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,38 @@
-module.exports = {
-  env: {
-    production: {
-      plugins: ['transform-remove-console'],
-    },
-  },
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        root: ['./src'],
-        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
-      },
+module.exports = function(api) {
+  api.cache(true);
+  if (process.env.NODE_ENV !== 'development') {
+    return {
+      presets: ['module:metro-react-native-babel-preset'],
+      plugins: [
+        'transform-remove-console',
+        'ignite-ignore-reactotron',
+        [
+          'module-resolver',
+          {
+            root: ['./src'],
+            extensions: [
+              '.ios.js',
+              '.android.js',
+              '.js',
+              '.ts',
+              '.tsx',
+              '.json',
+            ],
+          },
+        ],
+      ],
+    };
+  }
+  return {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        },
+      ],
     ],
-  ],
+  };
 };
