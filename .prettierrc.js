5e3c41bd42463975cc47e2cf692b754a228bc80e
Italo Menezes <italomlporoca@hotmail.com>
2020-06-20 01:52:50 -0300

    Apply some configs to update typescript and linting

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   singleQuote: true,
-  trailingComma: 'es5',
   tabWidth: 2,
   printWidth: 80,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
 };
--- babel.config.js
+++ babel.config.js
@@ -1,41 +1,25 @@
-module.exports = function(api) {
-  api.cache(true);
-  // TODO fix bug: this code is breaking development environment and
-  // is needed for release builds
-  // if (process.env.NODE_ENV !== 'development') {
-  //   console.log('passa aqui')
-  //   return {
-  //     presets: ['module:metro-react-native-babel-preset'],
-  //     plugins: [
-  //       'transform-remove-console',
-  //       'ignite-ignore-reactotron',
-  //       [
-  //         'module-resolver',
-  //         {
-  //           root: ['./src'],
-  //           extensions: [
-  //             '.ios.js',
-  //             '.android.js',
-  //             '.js',
-  //             '.ts',
-  //             '.tsx',
-  //             '.json',
-  //           ],
-  //         },
-  //       ],
-  //     ],
-  //   };
-  // }
-  return {
-    presets: ['module:metro-react-native-babel-preset'],
-    plugins: [
-      [
-        'module-resolver',
-        {
-          root: ['./src'],
-          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          '@user': './src/',
         },
-      ],
+      },
     ],
-  };
+  ],
 };
